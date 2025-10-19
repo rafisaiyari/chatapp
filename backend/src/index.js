7a48acf64fc4cb94485e8c6b7ad0b07174f43f10
rafisaiyari <rafisaiyari@ymail.com>
2025-10-20 01:14:17 +0800

    fix 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
