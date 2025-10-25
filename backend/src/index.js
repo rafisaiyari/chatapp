0e8f19ebc79d7c72306642e076a86a7d71363a6b
rafisaiyari <rafisaiyari@ymail.com>
2025-10-25 17:42:28 +0800

    added / try for render

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
-dotenv.config()
-
 import express from "express";
 import {connectDB} from "./lib/db.js";
 import cors from "cors";
@@ -14,14 +12,13 @@ import cookieParser from "cookie-parser";
 
 import {app, server} from "./lib/socket.js";
 
-
+dotenv.config()
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
-app.use(express.json());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -33,7 +30,7 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
